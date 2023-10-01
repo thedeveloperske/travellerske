8c6d88fe2dd1b857f21fa2481d9028ca7b68219d
TheDevelopersKE <thedeveloperske@gmail.com>
2023-10-01 17:53:52 +0300

    updated allowed hosts

--- travellerske/settings.py
+++ travellerske/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1r+yd4ee-zdu14z&*udip$ajo!-c&(x8uh=qcq%%c(%f(pld@n
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
